4e6797683500d6ef57b97bae0743f5c038d22de1
mmmari4ka <109090925+mmmari4ka@users.noreply.github.com>
2022-08-10 18:15:15 +0200

    Debugged API

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ function formatDate(timestamp) {
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  let day = days[date.getDay()];
   let days = [
     "Sunday",
     "Monday",
@@ -18,6 +17,8 @@ function formatDate(timestamp) {
     "Friday",
     "Saturday",
   ];
+  let day = days[date.getDay()];
+
   return `${day} ${hours}:${minutes}`;
 }
 
@@ -36,20 +37,23 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   speedElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute =
-    ("src", `http://openweathermap.org/img/wn/$`response.data.weather[0].icon`@2x.png`);
-  iconElement.setAttribute = ("alt", response.data.weather[0].description);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function search(city) {
-let apiKey = "21a82c254282e7f41002f03d1295cbaa";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+  let apiKey = "ac699745cab89bf5c0cc31378b9b9e84";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
-  let cityInputElement = document.querySelector(#city-input);
+  let cityInputElement = document.querySelector("#cityinput");
   search(cityInputElement.value);
 }
 
